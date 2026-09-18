db6e0aceb712df2b3f651ca1098d05f5abdebca9
Yang Yahan <yahanyang46@outlook.com>
2026-09-18 17:59:22 +0800

    Migrate site to 11ty layouts and collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,32 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.ignores.add("README.md");
 
-    eleventyConfig.addFilter("readableDate", (date) => {
-        return new Date(date).toLocaleDateString("en-US", {
-            year: "numeric",
-            month: "long",
-            day: "numeric",
-            timeZone: "Asia/Kuala_Lumpur"
-        });
+  eleventyConfig.addFilter("readableDate", (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      timeZone: "Asia/Kuala_Lumpur",
     });
+  });
+
+  eleventyConfig.addCollection("blogs", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("blogs/*.md").sort((a, b) => b.date - a.date);
+  });
+
+  eleventyConfig.addCollection("writeups", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("writeups/*.md").sort((a, b) => b.date - a.date);
+  });
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site",
+    },
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+  };
 };
